457ac143d34268430c7c6841dbbf087101f90a85
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-07-31 16:23:00 -0600

    Put key exports into an object

--- keys.js
+++ keys.js
@@ -1,13 +1,18 @@
 console.log('Keys are loaded....');
 
-exports.twitter = {
+const twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
+const spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+module.exports = {
+    twitter: twitter,
+    spotify: spotify
+}
