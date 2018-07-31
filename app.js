b72d5603a06f9ab383d040c64cec57a41c3a390a
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-07-31 16:18:53 -0600

    Scaffolding for bringing it all into an app module

---
+++ app.js
@@ -0,0 +1,17 @@
+/*  Liri JS
+ *  By Schuyler Ankele
+ *  A simple CLI app to access Twitter, Spotify and OMDB
+ *
+ */
+
+// Require block
+
+
+
+// Init modules
+
+// Call prompts
+
+// Run it
+
+
