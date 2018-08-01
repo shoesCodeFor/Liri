c80113b00398a4717aa6604ab362a82b40088669
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-08-01 16:38:41 -0600

    Spotify Functions into a module

---
+++ SpotifyToLog.js
@@ -0,0 +1,29 @@
+const keys = new API_Keys();
+const spotifyClient = new Spotify(keys.spotify);
+
+
+class SpotifyToLog{
+    constructor(){
+        this.spotifySearch = songName => {
+            spotifyClient.search({ type: 'track', query: songName}, function(err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                let songData = data.tracks.items[0];
+                console.log('************ Spotify Song Data ***************');
+                // Console log the first one....
+                // DEBUG: console.log(songData);
+                // Artist(s)
+                console.log(`Artist(s): ${songData.album.artists[0].name}`);
+                // The song's name
+                console.log(`Song Name: ${songData.name}`);
+                // A preview link of the song from Spotify
+                console.log(`Preview URL: ${songData.preview_url}`);
+                // The album that the song is from
+                console.log(`Album: ${songData.album.name}`);
+            });
+        };
+    }
+}
+
+module.exports = SpotifyToLog;
