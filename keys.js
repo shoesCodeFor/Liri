5dc902257f6e6cc30bbdea7834f511d09265a2f1
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-08-02 03:51:43 -0600

    Fix key exports

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 console.log('Keys are loaded....');
 
+// We'll dump this once we have verified the other two work
 class API_Keys{
     constructor(){
         this.twitter = {
@@ -16,4 +18,26 @@ class API_Keys{
     }
 }
 
-module.exports = API_Keys;
+class SpotifyAPI{
+    constructor(){
+        this.key = {
+            id: process.env.SPOTIFY_ID,
+            secret: process.env.SPOTIFY_SECRET
+        };
+    }
+}
+
+class TwitterAPI{
+    constructor(){
+        this.key = {
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        };
+    }
+}
+
+
+
+module.exports = [SpotifyAPI, TwitterAPI];
